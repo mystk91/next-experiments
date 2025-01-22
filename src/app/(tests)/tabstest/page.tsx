8e1d848776e53806df1c651423ee8235b5354bb5
mystk91 <90066188+mystk91@users.noreply.github.com>
2025-01-22 14:56:46 -0500

    tab component
    created a basic tab component that has an effect telling you which tab is open

---
+++ src/app/(tests)/tabstest/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css";
+import Tabs from "@/app/ui/Tabs/Tabs 1/tabs";
+
+export default function Page() {
+  return (
+    <div
+      style={{
+
+      }}
+    >
+      <Tabs />
+    </div>
+  );
+}
---
+++ src/app/ui/Tabs/Tabs 1/tabs.tsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import classNames from "classnames";
+import styles from "./tabs.module.css";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+// Creates a panel which will be displayed when we click a tab
+function TabPanel({ children, index, value }: TabPanelProps) {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+    >
+      {value === index && <div className={styles.tab_panel}>{children}</div>}
+    </div>
+  );
+}
+
+interface TabsProps {}
+
+export default function Tabs({}: TabsProps) {
+  // Creates a ref array to hold the button elements for each tab
+  const tabRefs = useRef<(HTMLButtonElement | null)[]>([]);
+
+  const [currentTab, setCurrentTab] = useState(0);
+  const [underlineStyle, setUnderlineStyle] = useState({
+    width: tabRefs.current[currentTab]?.clientWidth,
+    left: tabRefs.current[currentTab]?.offsetLeft,
+  });
+
+  // Moves the underline
+  useEffect(() => {
+    const tabElement = tabRefs.current[currentTab];
+    if (tabElement) {
+      const rect = tabElement.getBoundingClientRect();
+      const parent = tabElement.parentElement;
+      const parentRect = parent?.getBoundingClientRect();
+      setUnderlineStyle({
+        width: rect.width,
+        left: rect.left - parentRect!.left + parent!.scrollLeft,
+      });
+    }
+  }, [currentTab]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.tabs} role="tablist">
+        {[
+          "Overview",
+          "Details",
+          "Actions",
+          "This is a long tab",
+          "Summary",
+          "Analyze",
+          "More Words",
+        ].map((label, index) => (
+          <button
+            key={index}
+            className={classNames(styles.tab, {
+              [styles.active]: currentTab === index,
+            })}
+            onClick={() => setCurrentTab(index)}
+            ref={(el) => {
+              tabRefs.current[index] = el;
+            }}
+            role="tab"
+            aria-controls={`tabpanel-${index}`}
+            aria-selected={currentTab === index}
+          >
+            {label}
+          </button>
+        ))}
+        <div className={styles.underline} style={underlineStyle}></div>
+      </div>
+
+      <div className={styles.tab_panels}>
+        <TabPanel value={currentTab} index={0}>
+          <div>Panel 1 is cool</div>
+        </TabPanel>
+        <TabPanel value={currentTab} index={1}>
+          <div>This is panel 2</div>
+        </TabPanel>
+        <TabPanel value={currentTab} index={2}>
+          <div>Third is this panel</div>
+        </TabPanel>
+        <TabPanel value={currentTab} index={3}>
+          <div>4th best panel</div>
+        </TabPanel>
+        <TabPanel value={currentTab} index={4}>
+          <div>Panel 5</div>
+        </TabPanel>
+        <TabPanel value={currentTab} index={5}>
+          <div>Computing...</div>
+        </TabPanel>
+        <TabPanel value={currentTab} index={6}>
+          <div>Yes we need more. Always more.</div>
+        </TabPanel>
+      </div>
+    </div>
+  );
+}
