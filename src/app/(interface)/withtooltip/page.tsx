7c82b9b97c155b41f19e8d8ab2a1f400ad01ce68
mystk91 <90066188+mystk91@users.noreply.github.com>
2025-05-14 00:03:00 -0400

    Good HoverMenu
    Made a good simple hoverMenu without the zoom bugs my previous versions had, also simpler logic

--- src/app/(interface)/withtooltip/page.tsx
+++ src/app/(interface)/withtooltip/page.tsx
@@ -8,26 +8,46 @@ import HoverMenuBetter from "@/app/ui/HoverMenus/HoverMenu Wrapper 2/hoverMenuWr
 import HoverMenuFlexible from "@/app/ui/HoverMenus/HoverMenu Wrapper 3/hoverMenuWrapper";
 import SimpleHover from "@/app/ui/HoverMenus/HoverMenu Wrapper Simple/hoverMenu";
 import Navbar from "@/app/ui/Navbars/Sidebar-Menu-Good-1/navbar";
+import Button from "@/app/ui/Buttons/Button Set 1/button";
+
+export function SomeCard() {
+  return <div className={styles.some_card}>{`These are words`}</div>;
+}
 
 export default function Page() {
   return (
-    <SimpleHover
-      content={<Navbar />}
-      direction="bottom"
-      offset={10}
-      shift="none"
-      extraShift={0}
-    >
-      <Card
-        height="40.0rem"
-        width="28.0rem"
-        src="/images/cards/candle.jpg"
-        alt="A lit lavender candle next to a piece of lavender and wax crystals."
-        headline="Lavender Candle"
-        description="One of the best know aromatherapy scents for relaxation is lavender. That's because lavender is a scent that naturally promotes calm. So if you had a stressful day, kick off your shoes, turn on some soothing music, and light a lavender candle. The scent will help relax you, could improve your mood, and help reduce anxiety."
-        button={<button>{"Add to Bag"}</button>}
-      />
-    </SimpleHover>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <SimpleHover
+          content={<SomeCard />}
+          /*
+          content={<Navbar />}
+          content={
+            <Button
+              text="Click Me!"
+              variant="primary"
+              style={{ backgroundColor: "rgb(140, 140, 140)" }}
+            />
+          }
+            */
+          direction="bottom"
+          offset={4}
+          align="left"
+          shiftPixels={0}
+          shiftPercent={0}
+        >
+          <Card
+            height="40.0rem"
+            width="28.0rem"
+            src="/images/cards/candle.jpg"
+            alt="A lit lavender candle next to a piece of lavender and wax crystals."
+            headline="Lavender Candle"
+            description="One of the best know aromatherapy scents for relaxation is lavender. That's because lavender is a scent that naturally promotes calm. So if you had a stressful day, kick off your shoes, turn on some soothing music, and light a lavender candle. The scent will help relax you, could improve your mood, and help reduce anxiety."
+            button={<button>{"Add to Bag"}</button>}
+          />
+        </SimpleHover>
+      </div>
+    </div>
   );
 }
 /*
--- src/app/ui/HoverMenus/HoverMenu Wrapper Simple/hoverMenu.tsx
+++ src/app/ui/HoverMenus/HoverMenu Wrapper Simple/hoverMenu.tsx
@@ -1,30 +1,26 @@
 "use client";
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useCallback,
-  ReactNode,
-} from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import styles from "./hoverMenu.module.css";
 import classNames from "classnames";
 
 /*  Displays a menu when you hover over the child element
- *    children - the items which the hover event will be given to
- *    content - the inputed menu component / html
- *    direction - the direction which the menu will appear
- *    shift?         - shifts the menu towards one side, should be perpendicular of "direction"
- *    relativeShift  - in percent, shifts the menu by a percent of the hovered child's width / height
- *    extraShift?    - shifts the menu by an extra amount, in rem
+ *    children       - the items which the hover event will be given to
+ *    content        - the inputed menu component / html
+ *    direction      - the direction which the menu will appear
+ *    offset         - adds a gap between the children and the content, in px
+ *    align?         - aligns the menu on a side of the children, flows the opposite direction, should be perpendicular of "direction"
+ *    shiftPixels?   - shifts the menu in px
+ *    shiftPercent?  - shifts the menu by a percent of the child's width / height, 0 to 100
+ *    extraShift?    - shifts the menu by an extra amount, in px
  */
 interface HoverMenuProps {
   children: React.ReactNode;
   content: React.ReactNode;
   direction: "top" | "right" | "bottom" | "left";
   offset?: number;
-  shift?: "none" | "top" | "right" | "left" | "bottom";
-  relativeShift?: number;
-  extraShift?: number;
+  align?: "middle" | "top" | "right" | "left" | "bottom";
+  shiftPixels?: number;
+  shiftPercent?: number;
   role?: string;
   ariaLabel?: string;
 }
@@ -34,27 +30,31 @@ export default function HoverMenu({
   content,
   direction = "bottom",
   offset = 0.0,
-  shift = "none",
-  extraShift = 0.0,
-  relativeShift = 0,
+  align = "middle",
+  shiftPixels = 0.0,
+  shiftPercent = 0,
   role = "menu",
   ariaLabel = "Menu",
 }: HoverMenuProps) {
   const [active, setActive] = useState(false);
   const [appear, setAppear] = useState(false);
-  const hoverMenuRef = useRef<null | HTMLDivElement>(null);
+
   const childrenRef = useRef<null | HTMLDivElement>(null);
 
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
-
-  const [position, setPosition] = useState({
-    transform: "",
+  const positionObject = {
+    top: "",
+    right: "",
+    bottom: "",
+    left: "",
     paddingTop: "",
     paddingRight: "",
     paddingBottom: "",
     paddingLeft: "",
-  });
+    transform: "",
+  };
+  const [position, setPosition] = useState(positionObject);
 
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
   //Adds the menu
   function addMenu() {
     if (timerRef.current) {
@@ -70,7 +70,7 @@ export default function HoverMenu({
       clearTimeout(timerRef.current);
     }
     setAppear(false);
-    timerRef.current = setTimeout(() => setActive(false), 400);
+    timerRef.current = setTimeout(() => setActive(false), 300);
   }
 
   //Clears the timer
@@ -82,11 +82,12 @@ export default function HoverMenu({
     };
   }, []);
 
-  //Moves the tooltip to one of the sides when it becomes active
+  //Moves the menu to one of the sides when it becomes active
   useEffect(() => {
-    if (hoverMenuRef.current && childrenRef.current) {
-      let positionCopy = { ...position };
+    if (childrenRef.current) {
+      let positionCopy = { ...positionObject };
 
+      // Adds space between the menu and the children
       const paddingDirections: Record<
         "top" | "right" | "bottom" | "left",
         "paddingTop" | "paddingRight" | "paddingBottom" | "paddingLeft"
@@ -98,13 +99,103 @@ export default function HoverMenu({
       };
       positionCopy[paddingDirections[direction]] = `${offset}rem`;
 
-      //translate instead
+      // The style for each direction
+      const distanceStyles: Record<
+        "top" | "right" | "bottom" | "left",
+        {
+          direction: "top" | "right" | "bottom" | "left";
+          distance: string;
+        }
+      > = {
+        top: {
+          direction: "bottom",
+          distance: `${childrenRef.current.clientHeight}px`,
+        },
+        right: {
+          direction: "left",
+          distance: `${childrenRef.current.clientWidth}px`,
+        },
+        bottom: {
+          direction: "top",
+          distance: `${childrenRef.current.clientHeight}px`,
+        },
+        left: {
+          direction: "right",
+          distance: `${childrenRef.current.clientWidth}px`,
+        },
+      };
+      const distanceStyle = distanceStyles[direction];
+      positionCopy[distanceStyle.direction] = distanceStyle.distance;
 
-
-
-
-
-   
+      if (align === "middle") {
+        //Takes the "direction" input
+        const shiftStyles: Record<
+          "top" | "right" | "bottom" | "left",
+          {
+            direction: "top" | "right" | "bottom" | "left";
+            distance: string;
+            transform: string;
+          }
+        > = {
+          top: {
+            direction: "left",
+            distance: `50%`,
+            transform: `translateX(-50%)`,
+          },
+          right: {
+            direction: "top",
+            distance: `50%`,
+            transform: `translateY(-50%)`,
+          },
+          bottom: {
+            direction: "left",
+            distance: `50%`,
+            transform: `translateX(-50%)`,
+          },
+          left: {
+            direction: "top",
+            distance: `50%`,
+            transform: `translateY(-50%)`,
+          },
+        };
+        const shiftStyle = shiftStyles[direction];
+        positionCopy[shiftStyle.direction] = shiftStyle.distance;
+        positionCopy.transform = shiftStyle.transform;
+      } else {
+        //Takes the "align" input
+        const shiftStyles: Record<
+          "top" | "right" | "bottom" | "left",
+          {
+            direction: "top" | "right" | "bottom" | "left";
+            distance: string;
+            transform: string;
+          }
+        > = {
+          top: {
+            direction: "top",
+            distance: `${shiftPercent}%`,
+            transform: `translateY(${shiftPixels}px)`,
+          },
+          right: {
+            direction: "right",
+            distance: `${shiftPercent}%`,
+            transform: `translateX(${-shiftPixels}px)`,
+          },
+          bottom: {
+            direction: "bottom",
+            distance: `${shiftPercent}%`,
+            transform: `translateY(${-shiftPixels}px)`,
+          },
+          left: {
+            direction: "left",
+            distance: `${shiftPercent}%`,
+            transform: `translateX(${shiftPixels}px)`,
+          },
+        };
+        const shiftStyle = shiftStyles[align];
+        positionCopy[shiftStyle.direction] = shiftStyle.distance;
+        positionCopy.transform = shiftStyle.transform;
+      }
       setPosition(positionCopy);
     }
     return () => {};
@@ -112,14 +203,16 @@ export default function HoverMenu({
 
   return (
     <div
-      className={styles.hoverMenuWrapper}
+      ref={childrenRef}
+      aria-expanded={active}
       onMouseOver={addMenu}
       onMouseLeave={removeMenu}
+      className={styles.expandable}
     >
+      {children}
       {active && (
         <div
-          ref={hoverMenuRef}
-          className={classNames(styles.hoverMenu, {
+          className={classNames(styles.menu, {
             [styles.appear]: appear,
           })}
           role={role}
@@ -129,12 +222,9 @@ export default function HoverMenu({
             ...styles,
           }}
         >
-          <div className={styles.content}>{content}</div>
+          {content}
         </div>
       )}
-      <div ref={childrenRef} aria-expanded={active}>
-        {children}
-      </div>
     </div>
   );
 }
