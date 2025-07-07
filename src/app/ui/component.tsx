d7d42a472a5f45bcfd4d5421c2f4bc69e8c3a6fd
mystk91 <90066188+mystk91@users.noreply.github.com>
2025-07-07 02:14:42 -0400

    small edit

--- src/app/ui/component.tsx
+++ src/app/ui/component.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./component.module.css";
+import classNames from "classnames";
 
 interface ComponentProps {
   style?: React.CSSProperties;
@@ -11,7 +12,7 @@ interface ComponentProps {
 export default function Component({ style }: ComponentProps) {
   const [property, setProperty] = useState<JSX.Element>(<div>Welcome!</div>);
 
-  //componentDidMount, runs when component mounts, then componentDismount
+  //componentDidMount, runs when component mounts and returns on dismount
   useEffect(() => {
     return () => {};
   }, []);
